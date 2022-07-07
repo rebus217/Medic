e622f77d5978b37cbbb36380090721565885f7f6
Maks <palinov.ma@bokus.ru>
2022-07-07 17:21:39 +0300

    go home

--- src/blocks/headerContats/HeaderContacts.jsx
+++ src/blocks/headerContats/HeaderContacts.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './HeaderContacts.sass'
+import faceBookIcon from '../../icons/iconmonstr-facebook-4.svg'
+import linkedinIcon from '../../icons/iconmonstr-linkedin-4.svg'
+import twitterIcon from '../../icons/iconmonstr-twitter-4.svg'
 
 const HeaderContacts = () => {
     return (
         <div className={'contacts'}>
-            <div className={'contacts__icons'}>
-               <img src={'../../src/iconmonstr-facebook-4.svg'}/>
+            <div className={'contacts__icons-container'}>
+                <img src={faceBookIcon} alt={'faceBookIcon'} className={'contacts__icon'}/>
+                <img src={twitterIcon} alt={'twitterIcon'} className={'contacts__icon'}/>
+                <img src={linkedinIcon} alt={'linkedinIcon'} className={'contacts__icon'}/>
             </div>
             
         </div>
