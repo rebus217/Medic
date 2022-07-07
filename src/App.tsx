2aa54ec39a24b794632d4e0e6d272e2872539d7e
Rebus <palinov.ma@bokus.ru>
2022-07-07 22:09:44 +0300

    remove eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.sass';
-import HeaderContacts from "./blocks/headerContats/HeaderContacts";
+import SocialNetworks from "./blocks/social-networks/Social-networks";
+// @ts-ignore
+import logo from "../src/icons/logo.svg"
+import NavBar from "./blocks/nav-bar/nav-bar";
 
 function App() {
   return (
     <div>
       <section className='first-section container'>
           <div className={'first-section__contacts'}>
-              <HeaderContacts/>
+              <div className={'first-section__contacts-container container container_small'}>
+                      <SocialNetworks/>
+                  <a className={'first-section__e-mail'} href={'#'}>sales@medpro.com</a>
+              </div>
+          </div>
+          <div className={'first-section__header-container container container_small'}>
+              <img className={'first-section__logo'} src={logo} alt={'logo'}/>
+              <NavBar/>
           </div>
       </section >
     </div>
