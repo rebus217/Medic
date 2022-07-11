013b00a3705848c5b50f5d76f078fc2de1e9d5a7
Maks <palinov.ma@bokus.ru>
2022-07-11 17:13:24 +0300

    finish header and about us

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import AboutUsLeft from './about-us__left-content/about-us__Left';
 import './App.sass';
 import CarouselCustom from './blocks/carousel/carousel';
+import ForPhysicians from './blocks/for-physicians/for-physicians';
 import HeaderPhone from './blocks/header-phone/header-phone';
 import NavBar from './blocks/nav-bar/nav-bar';
 import SocialNetworks from './blocks/social-networks/Social-networks';
@@ -9,15 +11,15 @@ import logo from './icons/logo.svg';
 
 function App() {
   return (
-    <div className="container">
-      <div className="section-header">
+    <div className="custom-container">
+      <section id="header" className="section-header">
         <div className="blue-row ">
-          <div className="blue-row__content container_small">
+          <div className="blue-row__content custom-container_small">
             <SocialNetworks/>
             <a className={'section-header__e-mail'} href={'#'}>sales@medpro.com</a>
           </div>
         </div>
-        <div className="section-header__content-conteiner container_small">
+        <div className="section-header__content-conteiner custom-container_small">
           <img className={'section-header__logo'} src={logo} alt={'logo'}/>
           <div className="section-header__nav-bar-container">
             <NavBar/>
@@ -28,8 +30,19 @@ function App() {
         </div>
         <div className="section-header__carousel">
           <CarouselCustom/>
+          <div className="section-header__carousel-text">
+            <ForPhysicians/>
+          </div>
         </div>
-      </div>
+      </section>
+      <section id="about-us" className="about-us custom-container_small">
+        <div className="about-us__left-content">
+          <AboutUsLeft/>
+        </div>
+        <div className="about-us-right-content">
+
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/blocks/for-physicians/for-physicians.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './for-physicians.sass';
+
+export default function ForPhysicians() {
+  return (
+    <div className="for-physicians">
+      <p className="for-physicians__title">Fort Physicians</p>
+      <p className="for-physicians__promise">Our Promise</p>
+      <p className="for-physicians__text">90% First Pass payment within 30 days @up to 3% of collected revenue.</p>
+    </div>
+  );
+}
