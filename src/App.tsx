22459921589c8ec5f5cd02bfc1e411e8cbd0096d
Maks <palinov.ma@bokus.ru>
2022-07-12 12:00:41 +0300

    section 3 finish

--- src/App.tsx
+++ src/App.tsx
@@ -63,8 +63,25 @@ function App() {
           </div>
         </div>
         <div className={'benefits__right-content'}>
-
+          <div className={'benefits__text-container'}>
+            <p className={'benefits__title title'}>Benefits</p>
+            <p className={'benefits__selected-item'}>
+              For Physicians and Group Practice
+            </p>
+            <p className={'benefits__text'}>
+              • 	90% first pass payment within 30 days, at 3% Billing charges.<br/>
+              • 	Increase of 15-20% in monthly collected revenue.<br/>
+              • 	Expert teams with vast knowledge and experience who will follow up every claim to zero balance.<br/>
+              • 	Services spanning coding review, claims and payment processing, as well as Insurance and patient follow up.<br/>
+            </p>
+          </div>
+          <div className={'benefits__right-button'}>
+            <Button text={'READ MORE'} isBig={false} />
+          </div>
         </div>
+      </section>
+      <section id={'services'}>
+
       </section>
     </div>
   );
--- src/blocks/benefits__button-group/benefits__button-group.tsx
+++ src/blocks/benefits__button-group/benefits__button-group.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './benefits__button-group.sass';
 
 const BenefitsButtonGroup = () => {
+  let buttonFirst = 'buttons-group__button ';
+  let buttonSecond = 'buttons-group__button';
+  let buttonThird = 'buttons-group__button';
 
+  const [activeButton, setActiveButton] = useState(1);
 
+  if (activeButton === 1) buttonFirst += ' buttons-group__button_active';
+  if (activeButton === 2) buttonSecond += ' buttons-group__button_active';
+  if (activeButton === 3) buttonThird += ' buttons-group__button_active';
 
+    
   return (
     <div className={'buttons-group'}>
-      <div className={'buttons-group__button'}>
+      <div className={buttonFirst} onClick={() => setActiveButton(1)}>
           For Physicians and Group Practice:
       </div>
-      <div className={'buttons-group__button'}>
+      <div className={buttonSecond} onClick={() => setActiveButton(2)}>
           For Medical Billing Companies/Hospitals:
       </div>
-      <div className={'buttons-group__button'}>
+      <div className={buttonThird} onClick={() => setActiveButton(3)}>
           You Stay in Control:
       </div>
     </div>
