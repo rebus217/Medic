32e632ab27f0cdd8f7c323ad23e2c6aa379ec811
Maks <palinov.ma@bokus.ru>
2022-07-13 10:27:20 +0300

    section 4 end

--- src/blocks/section-services/section-service.tsx
+++ src/blocks/section-services/section-service.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './section-service.sass';
 import '../title/title.sass';
 import SectionServiceSpoiler from './section-service__spoiler/section-service__spoiler';
+import Button from '../button/button';
 
 
 const SectionService = () => {
@@ -15,6 +16,12 @@ const SectionService = () => {
       <div className={'section-service__right-content'}>
         <p className={'title'}>Security</p>
         <p className={'section-service__sub-title'}>POLI Compliance</p>
+        <div className={'section-service__right-image'}/>
+        <p className={'section-service__right-text'}>
+          We work with you to develop customized risk-management solutions covering information security,
+          business continuity and privacy regulations.
+        </p>
+        <Button text={'READ MORE'} isBig={false}/>
       </div>
     </div>
   );
--- src/blocks/section-services/section-service__spoiler/section-service__spoiler.tsx
+++ src/blocks/section-services/section-service__spoiler/section-service__spoiler.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './section-service__spoiler.sass';
-// import minus from '../../../icons/minus.svg';
-import plus from '../../../icons/plus.svg';
 import codingServicesImg from '../../../img/coding-services.png';
 
-const SectionServiceSpoiler = () => {
+const SectionServiceSpoiler: FC = () => {
+  function onClickSpoiler(e: React.MouseEvent<HTMLDivElement>) {
+    ((e.target as Element).parentNode?.parentNode?.childNodes[2] as Element).classList.toggle('service_item-active');
+    (e.target as Element).classList.toggle('service_icon-active');
+  }
+
   return (
     <div className={'service'}>
       <div className={'service__item'}>
         <div className={'service__item-title-container'}>
           <p className={'service__item-title'}>Coding Services</p>
-          <img src={plus} alt={'icon'} className={'service__item-icon'}/>
+          <div className={'service__item-icon'} onClick={(e)=>onClickSpoiler(e)}/>
+        </div>
+        <div className={'service__item-line'}/>
+        <div className={'service__item-content'}>
+          <img src={codingServicesImg} alt={'coding services'} className={'service__item-image'}/>
+          <div className={'service__item-text-container'}>
+            <p className={'service__item-text'}>
+              We offer high quality coding and auditing services to hospitals and physicians;
+              services extended for Anesthesia, Radiology, E&M and Surgery coding.
+            </p>
+            <p className={'service__item-read-more'}>
+              READ MORE
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={'service__item'}>
+        <div className={'service__item-title-container'}>
+          <p className={'service__item-title'}>Credentialing</p>
+          <div className={'service__item-icon'} onClick={(e)=>onClickSpoiler(e)}/>
+        </div>
+        <div className={'service__item-line'}/>
+        <div className={'service__item-content'}>
+          <img src={codingServicesImg} alt={'coding services'} className={'service__item-image'}/>
+          <div className={'service__item-text-container'}>
+            <p className={'service__item-text'}>
+              We offer high quality coding and auditing services to hospitals and physicians;
+              services extended for Anesthesia, Radiology, E&M and Surgery coding.
+            </p>
+            <p className={'service__item-read-more'}>
+              READ MORE
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={'service__item'}>
+        <div className={'service__item-title-container'}>
+          <p className={'service__item-title'}>HCC Maangement Services</p>
+          <div className={'service__item-icon'} onClick={(e)=>onClickSpoiler(e)}/>
         </div>
         <div className={'service__item-line'}/>
         <div className={'service__item-content'}>
